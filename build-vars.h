7dbc1aa67acf586cb7c68247515d6522631692e9
nikita <nazarov.nekit@mail.ru>
2021-04-17 14:12:12 +0300

    try to re-add kbd

---
+++ build-vars.h
@@ -0,0 +1,5 @@
+#pragma once
+
+const char * kGIT_SHA1 = "25bf4e68";
+const char * kGIT_DATE = "Sat Apr 17 13:43:47 2021";
+const char * kGIT_COMMIT_SUBJECT = "Merge branch 'rus_v1' of https://github.com/CheKecha19/kbd-audio into rus_v1";
